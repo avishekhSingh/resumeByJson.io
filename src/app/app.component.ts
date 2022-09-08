5ec4ae44978b891720fa2c9f95e4151d786a0ec6
avishekhSingh <100168458+avishekhSingh@users.noreply.github.com>
2022-09-08 18:58:39 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GetResumeService } from "./Services/get-resume.service"
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'resumeByJson';
+
+  resume: any = [];
+  person: any = []
+  skill: any;
+  projects: any;
+  education: any;
+  currentInfo: any;
+
+  constructor(private myResume: GetResumeService) {                //! create a variable myResume with data type "GetResumeService"
+    myResume.getData().subscribe((element) => {
+      this.resume = element;
+      console.log(this.resume[3].Project)
+
+      this.person = this.resume[0].Person;
+      this.skill = this.resume[1].Skill;
+      this.education = this.resume[2].Education;
+      this.projects = this.resume[3].Project;
+      this.currentInfo = this.resume;
+    })
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import { EducationComponent } from './education/education.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
+import { ObjectiveComponent } from './objective/objective.component';
+import { PersonalInfoComponent } from './personal-info/personal-info.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
+import { CurrentInfoComponent } from './current-info/current-info.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EducationComponent,
+    HobbiesComponent,
+    ObjectiveComponent,
+    PersonalInfoComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    CurrentInfoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
